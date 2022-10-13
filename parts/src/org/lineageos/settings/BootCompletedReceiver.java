ad0c59486abf314af9d91b24cb2313ae608a8e1b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-13 12:06:35 +0530

    sweet: parts: Restore MiSound scene on boot complete
    This seems to be necessary only for scene (and not
    other parameters).

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -34,7 +34,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
 
         // Dirac
-        DiracUtils.initialize(context);
+        DiracUtils.onBootCompleted(context);
 
         // Thermal Profiles
         ThermalUtils.startService(context);
