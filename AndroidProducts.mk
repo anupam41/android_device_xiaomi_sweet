6ce3c3e97bc303460670cf544a46e5fc86f90c8c
anupam41 <anupamsamanta41@gmail.com>
2022-11-22 20:39:03 +0100

    sweet: Welcome LegionOS
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The legionOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/legion_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    legion_sweet-user \
+    legion_sweet-userdebug \
+    legion_sweet-eng
